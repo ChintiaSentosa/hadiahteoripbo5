89bf3c4d26b027d18f48b1ef9222d361c1a5dcea
ChintiaSentosa <66993826+ChintiaSentosa@users.noreply.github.com>
2021-11-04 11:53:01 +0800

    Add files via upload

---
+++ volbola.java
@@ -0,0 +1,12 @@
+// Chintia Sentosa
+// 51019004
+
+import java.util.Scanner;
+
+public class volbola {
+	
+	void radius(int jari) {
+	System.out.println("Nilai Jari - jari Bola adalah: "+jari + " cm");
+	}
+	
+}
